8a09629b0760f421ee5399a7dc0dd9376833bf90
janelle <731364@student.dusd.net>
2024-03-14 13:47:41 -0700

    made new script and got the projectile to shoot
    i made a new script to spawn more animals and i got the projectile to shoot

--- P5Unit2BasicGamePlayAguayo/Assets/Scripts/MoveForward.cs
+++ P5Unit2BasicGamePlayAguayo/Assets/Scripts/MoveForward.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 40.0f;
+   
     // Start is called before the first frame update
     void Start()
     {
--- P5Unit2BasicGamePlayAguayo/Assets/Scripts/PlayerController.cs
+++ P5Unit2BasicGamePlayAguayo/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class P : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,10 @@ public class P : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
