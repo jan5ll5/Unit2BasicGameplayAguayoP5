9b4afd4871f76f95062344705c7cf5fe614bbccb
janelle <731364@student.dusd.net>
2024-03-18 13:47:54 -0700

    finished the first instructions now im on the bonus features
    now on bonus features, i did the first one (vertical player movement)

--- P5Unit2BasicGamePlayAguayo/Assets/Scripts/PlayerController.cs
+++ P5Unit2BasicGamePlayAguayo/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class P : MonoBehaviour
 {
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
@@ -29,6 +30,8 @@ public class P : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
